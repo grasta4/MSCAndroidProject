2f8b9c3420dcd6fbd03eb0882cff2d645dd3082d
JuliusLuebstorf <juliusluebstorf@gmail.com>
2019-11-19 22:30:32 +0100

    added small map to 'add task'

--- Project/app/src/main/java/com/example/msc/AddTaskActivity.java
+++ Project/app/src/main/java/com/example/msc/AddTaskActivity.java
@@ -3,16 +3,42 @@ package com.example.msc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AddTaskActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class AddTaskActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng unical = new LatLng(39.363134, 16.226203
+        );
+        mMap.addMarker(new MarkerOptions().position(unical).title("Marker at Unical"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(unical));
+    }
+
 
     // TODO: add location listener for 'insert location' text field
     // TODO: add ID field + id handler
     // TODO: add activity that confirms the location on the map with preview
+
 
 }
--- Project/app/src/main/java/com/example/msc/MapsActivity.java
+++ Project/app/src/main/java/com/example/msc/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.msc;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,6 +26,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -39,8 +42,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng unical = new LatLng(39.363134, 16.226203
+        );
+        mMap.addMarker(new MarkerOptions().position(unical).title("Marker at Unical"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(unical));
     }
+
 }
